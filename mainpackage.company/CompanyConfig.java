eb5da22c2f97cb77d4ad6b9868c66f8504f2755c
mberkehanergun <140403112+mberkehanergun@users.noreply.github.com>
2023-08-14 19:53:26 +0300

    Add files via upload

---
+++ mainpackage.company/CompanyConfig.java
@@ -0,0 +1,22 @@
+package mainpackage.company;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CompanyConfig {
+    private static int IANUM = 23;
+
+    public static int getIANUM() {
+        return IANUM;
+    }
+
+    public static void incrementAndSaveIANUMInDatabase(JdbcTemplate jdbcTemplate) {
+        try {
+            String updateQuery = "UPDATE COMPANYCONFIG SET IANUM = ?";
+            jdbcTemplate.update(updateQuery, ++IANUM);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
